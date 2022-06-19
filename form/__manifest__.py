0e78ee3af2e4a2d24877aa61163bef6c07f190bd
Nahid Aghababaeyan <nahid.aut@gmail.com>
2022-06-19 20:14:28 +0430

    ADD init and manifest files

---
+++ form/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    # Technical name is the name of the Folder Of Module Here is: form_module
+    # This is a App name that shows in the App List:
+    'name': 'Form',
+    'installable': True,
+    'application': True,
+    'sequence':-1,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/form_menu_views.xml',
+        'views/form_register_views.xml',
+    ],
+
+}
